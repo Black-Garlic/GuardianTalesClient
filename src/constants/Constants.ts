4c4ae2a34a88465e041c3b9c6f26a99070e355d0
Black-Garlic <hn1226@naver.com>
2023-04-11 22:44:33 +0900

    Constant, Type 추가

---
+++ src/constants/Constants.ts
@@ -0,0 +1,44 @@
+export enum PARTY_TYPE {
+  SINGLE = "SINGLE",
+  TRIPLE = "TRIPLE",
+}
+
+export enum STAGE {
+  UNIQUE = "유니크",
+  RARE_PROMOTION = "레어(승급)",
+  RARE = "레어",
+}
+
+export enum ELEMENT {
+  BASIC = "무",
+  LIGHT = "광",
+  DARK = "암",
+  FIRE = "화",
+  WATER = "수",
+  EARTH = "지",
+}
+
+export enum ROLE {
+  WARRIOR = "전사",
+  RANGED = "원거리 딜러",
+  TANKER = "탱커",
+  SUPPORT = "지원가",
+}
+
+export enum CHAI_TYPE {
+  ALL = "ALL",
+  DOWNED = "다운",
+  INJURED = "부상",
+  AIRBORNE = "에어본",
+}
+
+export enum WEAPON_TYPE {
+  ONE_HANDED_SWORD = "한손검",
+  TWO_HANDED_SWORD = "양손검",
+  RIFLE = "라이플",
+  BOW = "활",
+  STAFF = "스태프",
+  BASKET = "바구니",
+  GAUNTLET = "건틀렛",
+  CLAW = "클로",
+}
---
+++ src/typings/Hero.ts
@@ -0,0 +1,36 @@
+export interface Hero {
+  heroInfoId: number;
+  name: string;
+  englishName: string;
+  stage: string;
+  role: string;
+  element: string;
+  weaponInfo: Weapon;
+  weaponPhase: string;
+  weaponType: string;
+  partyBuff: string;
+  firstNormalAttackName: string;
+  firstNormalAttackDescription: string;
+  secondNormalAttackName: string;
+  secondNormalAttackDescription: string;
+  chainSkillName: string;
+  chainSkillStartType: string;
+  chainSkillEndType: string;
+  chainSkillDamage: number;
+  chainSkillDamageType: string;
+  chainSkillAdditionalEffect: string;
+  specialAbilityName: string;
+  specialAbilityDescription: string;
+}
+
+export interface Weapon {
+  name: string;
+  weaponType: string;
+  element: string;
+  weaponOption: string;
+  skillName: string;
+  chainType: string;
+  skillDamage: number;
+  skillRegenTime: number;
+  skillAdditionalEffect: string;
+}
